0a66df6f7fd0f3f88e1e683f1275e25a798d1af0
xyz <cbhature530@gmail.com>
2026-06-24 00:53:33 +0530

    use env variable for backend API URL

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
-
-const API = 'http://localhost:5000/api/auth'
+const API = `${import.meta.env.VITE_API_URL}/api/auth`
 
 export const loginAPI = (data) => axios.post(`${API}/login`, data)
 export const signupAPI = (data) => axios.post(`${API}/signup`, data)
